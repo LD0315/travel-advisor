956fbeda651fea1c406b2e0ce45aff0cf121d71a
Linlin Deng <linlin.deng@icemedia.com.au>
2023-01-12 17:45:25 +1000

    add google map api key

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import GoogleMapReact from 'google-map-react'
 import { Paper, Typography, useMediaQuery } from '@material-ui/core'
-//import LocationOutlinedIcon from '@material-ui/icons/LocationOutlined'
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined'
 import Rating from '@material-ui/lab'
 
 import useStyles from './styles'
@@ -15,7 +15,7 @@ const Map = () => {
     return (
        <div className={classes.mapContainer}>
             <GoogleMapReact
-                bootstrapURLKeys={{ key: '' }}
+                bootstrapURLKeys={{ key: 'AIzaSyBzLIYFABadzoiFS3ZFOm_OQdMcVLA9q4o' }}
                 defaultCenter={coordinates}
                 center={coordinates}
                 defaultZoom={14}
