648221dbcf395ff43fc5c39cb6c983590b90feb9
Linlin Deng <linlin.deng@icemedia.com.au>
2023-01-13 16:02:30 +1000

    update

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -10,20 +10,21 @@ const Map = ({ setCoordinates, setBounds, coordinates }) => {
     const classes = useStyles();
     const isMobile = useMediaQuery('(min-width:600px)');
 
-    const coords = { lat: 0, lng: 0 };
+    //const coords = { lat: 0, lng: 0 };
 
     return (
        <div className={classes.mapContainer}>
             <GoogleMapReact
                 bootstrapURLKeys={{ key: 'AIzaSyBzLIYFABadzoiFS3ZFOm_OQdMcVLA9q4o' }}
-                defaultCenter={coords}
-                center={coords}
+                defaultCenter={coordinates}
+                center={coordinates}
                 defaultZoom={14}
                 margin={[50, 50, 50, 50]}
                 options={''}
                 onChange={(e) => {
                     //console.log(e)
-                    setCoordinates({ lat: e.center.lat, lng: e.center.lng })
+                    setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+                    setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
                 }}
                 onChildClick={''}
             >
