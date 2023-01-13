6c38e29b4dce987261e266d7eb2b0acd07d034e9
Linlin Deng <linlin.deng@icemedia.com.au>
2023-01-13 16:31:14 +1000

    update api index

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
 
+/*
 const options = {
     params: {
       bl_latitude: '11.847676',
@@ -14,10 +15,22 @@ const options = {
       'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
     }
   };
+  */
 
 export const getPlacesData = async () => {
     try {
-        const { data: { data }} = await axios.get(URL, options);
+        const { data: { data }} = await axios.get(URL, {
+          params: {
+            bl_latitude: '11.847676',
+            tr_latitude: '12.838442',
+            bl_longitude: '109.095887',
+            tr_longitude: '109.149359',
+          },
+          headers: {
+            'X-RapidAPI-Key': '9da21e7aa8msh06c55481bdcf967p135afdjsn214c9d51df7b',
+            'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+          }
+        });
 
         return data;
     } catch (error) {
