63c0091fd5b7692c0251442aa6a372d5f6270a77
Linlin Deng <linlin.deng@icemedia.com.au>
2023-01-13 16:32:54 +1000

    update

--- src/api/index.js
+++ src/api/index.js
@@ -17,14 +17,14 @@ const options = {
   };
   */
 
-export const getPlacesData = async () => {
+export const getPlacesData = async (sw, ne) => {
     try {
         const { data: { data }} = await axios.get(URL, {
           params: {
-            bl_latitude: '11.847676',
-            tr_latitude: '12.838442',
-            bl_longitude: '109.095887',
-            tr_longitude: '109.149359',
+            bl_latitude: sw.lat,
+            tr_latitude: ne.lat,
+            bl_longitude: sw.lng,
+            tr_longitude: ne.lng,
           },
           headers: {
             'X-RapidAPI-Key': '9da21e7aa8msh06c55481bdcf967p135afdjsn214c9d51df7b',
