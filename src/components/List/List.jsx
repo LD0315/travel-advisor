db24d121c5c2d003a2c57bef0c3e7eb226321151
Linlin Deng <linlin.deng@icemedia.com.au>
2023-01-13 14:53:15 +1000

    update list places

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core'
+import PlaceDetails from '../PlaceDetails/PlaceDetails'
 
 import useStyles from './styles'
 
@@ -9,6 +10,12 @@ const List = () => {
     const [rating, setRating] = useState('');
 
     const places = [
+        { name: 'Cool Place' },
+        { name: 'Best Beer' },
+        { name: 'Best Steak' },
+        { name: 'Cool Place' },
+        { name: 'Best Beer' },
+        { name: 'Best Steak' },
         { name: 'Cool Place' },
         { name: 'Best Beer' },
         { name: 'Best Steak' },
@@ -34,6 +41,13 @@ const List = () => {
                     <MenuItem value={4.5}>Above 4.5</MenuItem>
                 </Select>
             </FormControl>
+            <Grid container spacing={3} className={classes.list}>
+                {places?.map((place, i) => (
+                    <Grid item key={i} xs={12}>
+                        <PlaceDetails place={place} />
+                    </Grid>
+                ))}
+            </Grid>
         </div>
     )
 }
